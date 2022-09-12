9ca786d75fa9c3070110069b9aa46232ef7f86c9
ShibinChristin <shibinchristin0@gmail.com>
2022-09-12 10:12:19 +0530

    Fixing Courier update

--- Courier.cpp
+++ Courier.cpp
@@ -3,7 +3,7 @@ void Courier::listOfOrders()
 {
     std::string location, choice;
     std::string Name, OrderID, Count, Courier, Type;
-    cout << "1.Kochi\n2.Ernakulam\n ";
+    cout << "1.Kochi\n2.Ernakulam\n";
 ch:
     cout << "Enter choice :";
     getline(cin >> ws, choice);
@@ -39,26 +39,30 @@ ch:
                     found = true;
                 }
             }
-            if (token.rfind("Name ", 0) == 0 && found)
+            if (token.rfind("Name ", 0) == 0)
             {
                 Name = token.substr(5);
             }
-            if (token.rfind("OrderID ", 0) == 0 && found)
+            if (token.rfind("OrderID ", 0) == 0)
             {
-                Name = token.substr(8);
+                OrderID = token.substr(8);
             }
-            if (token.rfind("Type ", 0) == 0 && found)
+            if (token.rfind("Type ", 0) == 0)
             {
                 Type = token.substr(5);
             }
-            if (token.rfind("Count ", 0) == 0 && found)
+            if (token.rfind("Count ", 0) == 0)
             {
                 Count = token.substr(5);
             }
             line.erase(0, pos + delimiter.length());
         }
-        if(found==false){
-            cout<<"Order ID "<<OrderID<<" | "<<"Name "<<Name<<" | "<<"Type "<<Type<<" | "<<"Count "<<Count<<endl;
+        if (found)
+        {
+            cout << "Order ID " << OrderID << " | "
+                 << "Name " << Name << " | "
+                 << "Type " << Type << " | "
+                 << "Count " << Count << endl;
             break;
         }
     }
@@ -68,23 +72,72 @@ ch:
 
 void Courier::StatusUpdate()
 {
-    std::string location, choice;
-    cout << "*********************Status Updater*********************\n";
-    cout << "1.Kochi\n2.Ernakulam\n ";
-ch:
-    cout << "Enter choice :";
-    getline(cin >> ws, choice);
-    if (choice == "1")
+    std::string Orderid;
+    listOfOrders();
+    cout << "Enter Order ID to be updated : ";
+    getline(cin >> ws, Orderid);
+    std::string OrderId, line;
+    bool flag = false;
+    int CourierChoice;
+    std::string delimiter = ";";
+    ifstream courier("orders.txt", ios::in);
+    ofstream temp("Temp.txt", ios::out);
+    while (std::getline(courier, line))
     {
-        location = "Kochi";
-    }
-    else if (choice == "2")
-    {
-        location = "Ernakulam";
-    }
-    else
-    {
-        std::cout << "       ===Not a valid choice !! Please retry.......===\n";
-        goto ch;
+        std::string OrderID, ProductID, Name, Type, Count, Status;
+        size_t pos = 0;
+        std::string token, OriginalLine = line;
+        while ((pos = line.find(delimiter)) != std::string::npos)
+        {
+            token = line.substr(0, pos);
+            if (token.rfind("OrderID ", 0) == 0)
+            {
+                OrderID = token.substr(8);
+                if (OrderID == Orderid)
+                {
+                    flag = true;
+                }
+            }
+            if (token.rfind("ProductID ", 0) == 0 && flag)
+            {
+                ProductID = token.substr(9);
+            }
+            if (token.rfind("Name ", 0) == 0 && flag)
+            {
+                Name = token.substr(5);
+            }
+            if (token.rfind("Type ", 0) == 0 && flag)
+            {
+                Type = token.substr(5);
+            }
+            if (token.rfind("Count ", 0) == 0 && flag)
+            {
+                Count = token.substr(6);
+            }
+            if (token.rfind("Status ", 0) == 0 && flag)
+            {
+                Status = token.substr(7);
+            }
+            line.erase(0, pos + delimiter.length());
+        }
+        if (flag)
+        {
+            temp << "OrderID " << OrderID << ";"
+                 << "ProductID " << ProductID << ";"
+                 << "Name " << Name << ";"
+                 << "Type " << Type << ";"
+                 << "Count " << Count << ";"
+                 << "Courier "
+                 << "Kochi"
+                 << ";"
+                 << "Status "
+                 << "Delivered"
+                 << ";" << endl;
+            flag = false;
+            break;
+        }
+        else{
+            temp<<OriginalLine<<endl;
+        }
     }
 }
--- Merchant.cpp
+++ Merchant.cpp
@@ -1,110 +1,4 @@
 #include "project.h"
-// void Products::addProduct()
-// {
-//         // std ::cout << "Enter the Merchant ID : ";
-//         // getline(std::cin >> ws, merchantId);
-//         std ::cout << "\nEnter the Product ID : ";
-//         getline(std::cin >> ws, productId);
-//         std ::cout << "\nEnter the Name of the Product : ";
-//         getline(std::cin >> ws, productName);
-//         std ::cout << "\nEnter the Price of the Product : ";
-//         getline(std::cin >> ws, productPrice);
-// choice:
-//         std ::cout << "\nEnter the Type of the Product \n1.Electronics\n2.Furniture\n3.Clothings : ";
-//         cin >> productChoice;
-//         if (productChoice == 1)
-//         {
-//                 productType = "Electronics";
-//         }
-//         else if (productChoice == 2)
-//         {
-//                 productType = "Furniture";
-//         }
-//         else if (productChoice == 3)
-//         {
-//                 productType = "Fashion";
-//         }
-//         else
-//         {
-//                 cout << "Wrong choice....Enter Again!!";
-//                 goto choice;
-//         }
-//         std ::cout << "\nThe number of available " << productType << " Products: ";
-//         getline(std::cin >> ws, productCount);
-//         fstream merchant_file;
-//         merchant_file.open("products.txt", std::ios::out | std::ios::app);
-//         merchant_file //<< "MerchantID " << merchantId << ";"
-//             << "ProductID " << productId << ";"
-//             << "Name " << productName << ";"
-//             << "Price " << productPrice << ";"
-//             << "Type " << productType << ";"
-//             << "Count " << productCount << ";" << endl;
-//         cout << "\n";
-//         merchant_file.close();
-//         cout << "Would you like to continue?(Y/N) ";
-//         std::string choice;
-//         getline(std::cin >> std::ws, choice);
-//         if (choice == "Y" || choice == "y")
-//         {
-//                 addProduct();
-//         }
-//         else
-//         {
-//                 merchantOptions();
-//         }
-// }
-// void Products::merchantOptions()
-// {
-//         cout << "*******************        MENU        ********************************\n\n";
-//         cout << "1.Add Products\n";
-//         cout << "2.View Products\n";
-//         cout << "2.Assign Delivery to Courier\n";
-//         cout << "3.Canceled Orders\n";
-//         cout << "4.Order Status\n"
-//              << endl;
-// }
-// ifstream MerchantDisplay("products.txt");
-// void Products::displayProducts()
-// {
-//         std::string subString;
-//         std::cout << "1.Electronics\n";
-//         std::cout << "2.Furniture\n";
-//         std::cout << "3.Clothings\n";
-//         std::cout << "Enter your choice : ";
-//         std::cin >> MerchantChoice;
-//         switch (MerchantChoice)
-//         {
-//         case 1:
-//                 subString = "Electronics";
-//                 MerchantCondition(subString);
-//                 break;
-
-//         case 2:
-//                 subString = "Furniture";
-//                 MerchantCondition(subString);
-
-//                 break;
-//         case 3:
-//                 subString = "Clothes";
-//                 MerchantCondition(subString);
-//                 break;
-//         }
-// }
-// /////////////////////////////////////////////////////////
-
-// void Products::MerchantCondition(std::string Sub)
-// {
-//         std::string line1;
-//         ifstream MerchantDisplay("products.txt");
-//         do
-//         {
-//                 if (line1.find(Sub) != std::string::npos)
-//                         // cout<<line<<"\n";
-//                         cout << line1 << endl;
-//         } while (getline(MerchantDisplay, line1));
-
-// }
-
 void Products::OrderStatusView()
 {
 
@@ -266,7 +160,6 @@ b:
 
         std::fstream merchantFile;
         merchantFile.open("products.txt", std::ios::in | std::ios::out | std::ios::app);
-        //     merchantFile << "Product Name "<< productName <<";" << "ProductId "<< productId << ";"<< "Product Price " << productPrice << ";" << "Product Type " << productType << ";"<< "ProductCount " << productCount << ";" << std::endl;
         merchantFile << "ProductID " << productId << ";Name " << productName << ";Price " << productPrice << ";Type " << productType << ";Count " << productCount << ";" << endl;
 
         writeData.close();
@@ -431,7 +324,6 @@ void Products::AssignCourier()
                         if (token.rfind("Status ", 0) == 0 && iffound)
                         {
                                 Status = token.substr(7);
-                                cout << Status << "Status";
                         }
                         line.erase(0, pos + delimiter.length());
                 }
@@ -441,6 +333,7 @@ void Products::AssignCourier()
                 case 1:
                         if (iffound)
                         {
+                                cout<<"Updated Successfully......"<<endl;
                                 temp << "OrderID " << OrderID << ";"
                                      << "ProductID " << ProductID << ";"
                                      << "Name " << Name << ";"
@@ -484,12 +377,12 @@ void Products::AssignCourier()
                         }
                         break;
                 default:
-                cout<<"Not Valid...."<<endl;
+                        cout << "Not Valid...." << endl;
                 }
         }
         courier.close();
         temp.close();
         remove("orders.txt");
-        rename("Temp.txt","orders.txt");
+        rename("Temp.txt", "orders.txt");
 }
 ////////////////////////////////////////////////////////////
